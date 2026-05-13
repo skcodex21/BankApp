7e4389e81b1b9a4e73be851d1a359e95b502692b
skcodex21 <dookornagbor@gmail.com>
2026-05-13 17:43:35 +0100

    Update account.py

--- account.py
+++ account.py
@@ -1,1 +1,21 @@
+class Account:
+    def __init__(self, owner, balance=0):
+        self.owner = owner
+        self.__balance = balance
 
+    def deposit(sef, amount):
+        if amount > 0:
+            self.__balance += amount
+            print(f"Deposited $ {amount}. New balance: ${self.__balance}")
+        else:
+          print("Your balance is less than zero")
+            
+    def withdraw(self, amount):
+        if 0 < amount <= self.__balance:
+          self.__balance -= amount
+          print(f"withdraw ${amount}. remaining: ${self.__balance}")
+        else:
+         print("Invalid withdrawal amount or insufficient funds.")
+    def get_balance(self):
+        """Public getter for the private__balance"""
+        return self.__balance
